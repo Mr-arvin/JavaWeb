3cc77aa844508da556963de5617fb995a9b7d938
Mr-arvin <arvin1234567890@foxmail.com>
2018-09-02 23:17:10 +0800

    defaultValue

--- springmvc/04requestparams/src/main/java/com/monkey1024/controller/ParamsController02.java
+++ springmvc/04requestparams/src/main/java/com/monkey1024/controller/ParamsController02.java
@@ -32,4 +32,15 @@ public class ParamsController02 {
         mv.setViewName("getParamsResult");
         return mv;
     }
+
+    //defaultValue 前台传过来为空，后台则默认"18"
+    @RequestMapping("/params04")
+    public ModelAndView getParams04(@RequestParam(name = "username") String t_username, @RequestParam(name = "age", defaultValue = "18") int t_age, @RequestParam(required = false) String password) throws Exception {
+        //方法的参数名需要跟前台页面中的表单里的input的name一致
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("username",t_username);
+        mv.addObject("age",t_age);
+        mv.setViewName("getParamsResult");
+        return mv;
+    }
 }
--- springmvc/04requestparams/src/main/webapp/jsp/regist.jsp
+++ springmvc/04requestparams/src/main/webapp/jsp/regist.jsp
@@ -36,5 +36,15 @@
     年龄：<input type="text" name="age">
     <input type="submit" value="提交">
 </form>
+<br>
+<br>
+/user/params03=4.do:
+<form action="/user/params04.do" method="post">
+<form action="/user/params04.do" method="post">
+    姓名：<input type="text" name="username">
+    <br>
+    年龄：<input type="text" name="age">
+    <input type="submit" value="提交">
+</form>
 </body>
 </html>
