a269d7464d18fbbbbd17290b9b1a4b1565376917
Mr-arvin <arvin1234567890@foxmail.com>
2018-09-19 23:35:07 +0800

    model接口，可以用来传递数据，模拟modelandview

---
+++ src/main/java/com/monkey1024/controller/ModelController.java
@@ -0,0 +1,33 @@
+package com.monkey1024.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * model接口，可以用来传递数据，模拟modelandview
+ *
+ */
+
+@Controller
+public class ModelController {
+    @RequestMapping("/modeldata.do")
+    public String modelData(Model model, String name) throws Exception {
+        model.addAttribute("username",name);
+        model.addAttribute(name);//类似于model.addAttribute("string",name);
+        return "modeldata";
+    }
+
+    @RequestMapping("/modeldatalist.do")
+    public String modelDataList(Model model) throws Exception {
+        List<Integer> integersList = new ArrayList<>();
+        integersList.add(1);
+        integersList.add(5);
+        integersList.add(3);
+        model.addAttribute("username",integersList);
+        return "modeldata";
+    }
+}
---
+++ target/05returnvalue/jsp/modeldata.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: xm
+  Date: 2018/9/19
+  Time: 下午10:53
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+${username}
+</body>
+</html>
