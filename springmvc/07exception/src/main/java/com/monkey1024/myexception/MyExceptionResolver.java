db7b5ac7d70e2bbd85999da6805aabde5df12cd7
Mr-arvin <arvin1234567890@foxmail.com>
2018-09-26 19:32:48 +0800

    自定义异常处理器   只要在程序中有异常抛出，那么就会执行该方法

--- springmvc/07exception/src/main/java/com/monkey1024/myexception/MyExceptionResolver.java
+++ springmvc/07exception/src/main/java/com/monkey1024/myexception/MyExceptionResolver.java
@@ -9,10 +9,12 @@ import javax.servlet.http.HttpServletResponse;
 /**
  *自定义异常处理器
  */
+//写一个类注册HandlerExceptionResolver这个接口，然后在springmvc.xml中把这个类注册一下MyExceptionResolver
 public class MyExceptionResolver implements HandlerExceptionResolver {
 
     /**
      * 只要在程序中有异常抛出，那么就会执行该方法
+     * 这样的好处是只要抛出异常，就执行这里，并且可以在里面判断（ex instanceof MyException)，更加自定义、自由
      * @param request
      * @param response
      * @param handler
@@ -25,6 +27,7 @@ public class MyExceptionResolver implements HandlerExceptionResolver {
         mv.addObject("ex",ex);
         //是指默认异常处理界面
         mv.setViewName("error/error");
+        //判断ex对象是否是MyException的实例
         if(ex instanceof MyException){
             //记录日志
             System.out.print("自定义");
@@ -32,6 +35,7 @@ public class MyExceptionResolver implements HandlerExceptionResolver {
             mv.setViewName("error/myerror");
         }else if(ex instanceof NullPointerException){
             //记录日志
+
         }
         return mv;
     }
